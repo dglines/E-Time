6e15fa2d9287d8f3a0299a6397a6c0a320bf0a5b
David Glines <dglines86@gmail.com>
2018-05-04 14:32:48 -0700

    created basic navigation bar (hamburger button thing).  items in nav bar are not yet clickable.

--- app/src/main/java/edu/tacoma/uw/css/group7/e_time/MainActivity.java
+++ app/src/main/java/edu/tacoma/uw/css/group7/e_time/MainActivity.java
@@ -1,13 +1,40 @@
 package edu.tacoma.uw.css.group7.e_time;
 
+import android.drm.DrmStore;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    // a reference to the drawer
+    private DrawerLayout mDrawrLayout;
+
+    // works as a listener for the drawer
+    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDrawrLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawrLayout, 0, 0);
+        mDrawrLayout.addDrawerListener(mToggle);
+
+        // sysnState allows opening and closing the nav menu
+        mToggle.syncState();
+        // enable a back button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // chechkif hamburger menu toggle selected
+        return mToggle.onOptionsItemSelected(item);
+    }
+
+
 }
