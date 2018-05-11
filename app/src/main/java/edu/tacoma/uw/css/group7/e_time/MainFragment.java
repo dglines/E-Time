3460c8018ad687cdce3015cc10b8932ffee13826
David Glines <dglines86@gmail.com>
2018-05-11 14:15:10 -0700

    transitions between home, recent and login.

--- app/src/main/java/edu/tacoma/uw/css/group7/e_time/MainFragment.java
+++ app/src/main/java/edu/tacoma/uw/css/group7/e_time/MainFragment.java
@@ -91,6 +91,7 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         final View v = inflater.inflate(R.layout.fragment_main, container, false);
 
         // thread used to update clock display
