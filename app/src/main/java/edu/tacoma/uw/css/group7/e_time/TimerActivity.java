d184f1c4b0cf6aa00023b65ba1cfb7d128d53c5c
Alexander Reid <csHourglass@live.com>
2018-05-11 14:27:24 -0700

    Added a fix to NullPointerException

---
+++ app/src/main/java/edu/tacoma/uw/css/group7/e_time/TimerActivity.java
@@ -0,0 +1,13 @@
+package edu.tacoma.uw.css.group7.e_time;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TimerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timer);
+    }
+}
